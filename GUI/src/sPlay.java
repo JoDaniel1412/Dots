fb80b8b24911ec54a1f36f62c5292b24715bef4a
[Fabian] <[ramirezfabian003@gmail.com]>
2018-08-23 13:03:44 -0600

    Creado el paquete Dots, iniciada la logica para que cada punto en el juego sea un boton independiente

--- GUI/src/sPlay.java
+++ GUI/src/sPlay.java
@@ -5,6 +5,7 @@ import javafx.scene.control.Label;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.text.Font;
 
 public class sPlay {
 
@@ -16,6 +17,7 @@ public class sPlay {
 
         // Tittle
         Label lTittle = new Label("Play");
+
 
         // Buttons
         Button bHost = new Button("Host Game");
