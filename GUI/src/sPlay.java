d00c8a6f0f3fa844a48958697cbf04a32dc5e1b4
Jesus <sandoval14yamir@hotmail.com>
2018-08-24 21:39:34 -0600

    Primera version del host/join menu Primer version del client/socket

--- GUI/src/sPlay.java
+++ GUI/src/sPlay.java
@@ -23,7 +23,7 @@ public class sPlay {
         Button bReturn = new Button("Return");
 
         bReturn.setOnAction(e -> Main.setScene(sMenu.run()));
-        bHost.setOnAction(e -> Main.setScene(sHost.run()));
+        bJoin.setOnAction(e -> Main.setScene(sJoin.run()));
 
 
         // Layouts
