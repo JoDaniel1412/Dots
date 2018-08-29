7b1885c25e8c107a6d9e99e3d2cd34e6a75a3508
[Fabian] <[ramirezfabian003@gmail.com]>
2018-08-29 12:01:13 -0600

    Added simpleList Class, again

---
+++ GUI/src/DOTS/Dots.java
@@ -0,0 +1,15 @@
+package DOTS;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import javafx.scene.text.Font;
+
+import java.awt.*;
+
+public class Dots extends javafx.scene.control.ToggleButton{
+
+    public Dots(){
+
+
+    }
+}
---
+++ GUI/src/LIST/simpleList.java
@@ -0,0 +1,78 @@
+package LIST;
+
+public class simpleList<T> {
+    private Node<T> first;
+    private int large;
+
+    public simpleList(){
+        this.first = null;
+        this.large = 0;
+    }
+
+    public void addAtEnd(T value){
+        if(this.isEmpty()){
+            this.first = new Node<T>(value);
+            this.large += 1;
+        }
+        else{
+            Node temporal = this.first;
+            while (temporal.getNext() != null){
+                temporal =  temporal.getNext();
+            }
+            temporal.setNext(new Node<T>(value));
+            this.large += 1;
+        }
+    }
+
+    public boolean isEmpty(){
+        Node next = this.getFirst();
+        if(next == null){
+            return true;
+        }
+        else {
+            return false;
+        }
+    }
+
+    public Node<T> getFirst() {
+        return first;
+    }
+    public void showLarge() {
+        System.out.println(this.getLarge());
+    }
+    public int getLarge() {
+        return large;
+    }
+
+    public void clearOut(){
+        this.first = null;
+    }
+
+    public void showList(){
+        Node temporal = this.first;
+        while (temporal != null){
+            System.out.println(temporal.getValue());
+            temporal = temporal.getNext();
+        }
+    }
+    public void delete(int index){
+        if (index < this.getLarge()){
+            int i = 0;
+            Node temporal = this.first;
+            while (temporal.getNext() != null){
+                i += 1;
+                if (i == index){
+                    temporal.setNext(temporal.getNext().getNext());
+                    this.large -= 1;
+                    break;
+                }
+                else{
+                    temporal = temporal.getNext();
+                }
+            }
+        }
+        else{
+            System.out.println("List out of index");
+        }
+    }
+}
