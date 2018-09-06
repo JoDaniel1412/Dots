575e72712671b1a64989da6489dba575bfee5374
[Fabian] <[ramirezfabian003@gmail.com]>
2018-09-06 01:24:17 -0600

    Fixed getters and settets in dot class

--- GameLogic/src/lists/Dot.java
+++ GameLogic/src/lists/Dot.java
@@ -1,98 +1,98 @@
 package lists;
 
 public class Dot extends Node{
-    private boolean next;
-    private boolean right;
-    private boolean left;
-    private boolean top;
-    private boolean bottom;
-    private boolean top_right;
-    private boolean top_left;
-    private boolean bottom_right;
-    private boolean bottom_left;
+    private boolean nextState;
+    private boolean rightState;
+    private boolean leftState;
+    private boolean topState;
+    private boolean bottomState;
+    private boolean top_rightState;
+    private boolean top_leftState;
+    private boolean bottom_rightState;
+    private boolean bottom_leftState;
 
     public Dot (){
-        this.next = false;
-        this.right = false;
-        this.left = false;
-        this.top = false;
-        this.bottom = false;
-        this.top_right = false;
-        this.top_left = false;
-        this.bottom_right = false;
-        this.bottom_left = false;
+        this.nextState = false;
+        this.rightState = false;
+        this.leftState = false;
+        this.topState = false;
+        this.bottomState = false;
+        this.top_rightState = false;
+        this.top_leftState = false;
+        this.bottom_rightState = false;
+        this.bottom_leftState = false;
     }
 
-    public boolean isNext() {
-        return next;
+    public boolean isNextState() {
+        return nextState;
     }
 
-    public void setNext(boolean next) {
-        this.next = next;
+    public void setNextState(boolean next) {
+        this.nextState = next;
     }
 
-    public boolean isRight() {
-        return right;
+    public boolean isRightState() {
+        return rightState;
     }
 
-    public void setRight(boolean right) {
-        this.right = right;
+    public void setRightState(boolean right) {
+        this.rightState = right;
     }
 
-    public boolean isLeft() {
-        return left;
+    public boolean isLeftState() {
+        return leftState;
     }
 
-    public void setLeft(boolean left) {
-        this.left = left;
+    public void setLeftState(boolean left) {
+        this.leftState = left;
     }
 
-    public boolean isTop() {
-        return top;
+    public boolean isTopState() {
+        return topState;
     }
 
-    public void setTop(boolean top) {
-        this.top = top;
+    public void setTopState(boolean top) {
+        this.topState = top;
     }
 
-    public boolean isBottom() {
-        return bottom;
+    public boolean isBottomState() {
+        return bottomState;
     }
 
-    public void setBottom(boolean bottom) {
-        this.bottom = bottom;
+    public void setBottomState(boolean bottom) {
+        this.bottomState = bottom;
     }
 
-    public boolean isTop_right() {
-        return top_right;
+    public boolean isTop_rightState() {
+        return top_rightState;
     }
 
-    public void setTop_right(boolean top_right) {
-        this.top_right = top_right;
+    public void setTop_rightState(boolean top_right) {
+        this.top_rightState = top_right;
     }
 
-    public boolean isTop_left() {
-        return top_left;
+    public boolean isTop_leftState() {
+        return top_leftState;
     }
 
-    public void setTop_left(boolean top_left) {
-        this.top_left = top_left;
+    public void setTop_leftState(boolean top_left) {
+        this.top_leftState = top_left;
     }
 
-    public boolean isBottom_right() {
-        return bottom_right;
+    public boolean isBottom_rightState() {
+        return bottom_rightState;
     }
 
-    public void setBottom_right(boolean bottom_right) {
-        this.bottom_right = bottom_right;
+    public void setBottom_rightState(boolean bottom_right) {
+        this.bottom_rightState = bottom_right;
     }
 
-    public boolean isBottom_left() {
-        return bottom_left;
+    public boolean isBottom_leftState() {
+        return bottom_leftState;
     }
 
-    public void setBottom_left(boolean bottom_left) {
-        this.bottom_left = bottom_left;
+    public void setBottom_leftState(boolean bottom_left) {
+        this.bottom_leftState = bottom_left;
     }
 
 }
