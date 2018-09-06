c8ba9f41216e61da81d4dc0fff8b39d4af117ef7
[Fabian] <[ramirezfabian003@gmail.com]>
2018-09-06 01:04:51 -0600

    Added Dot class and set up the arguments

---
+++ GameLogic/src/lists/Dot.java
@@ -0,0 +1,15 @@
+package lists;
+
+public class Dot extends Node{
+    private boolean next;
+    private boolean right;
+    private boolean left;
+    private boolean top;
+    private boolean bottom;
+    private boolean top_right;
+    private boolean top_left;
+    private boolean bottom_right;
+    private boolean bottom_left;
+
+
+}
