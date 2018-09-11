253862dbfcda884efaa15908370100619c7f1726
[Fabian] <[ramirezfabian003@gmail.com]>
2018-09-11 08:40:32 -0600

    Modified methods to verify if there is diagonals before check squares and checks if there is a triangle when a non diagonal line is draw in the class Vertical Checker

--- GameLogic/src/PointsChecker/verticalChecker.java
+++ GameLogic/src/PointsChecker/verticalChecker.java
@@ -13,45 +13,49 @@ public class verticalChecker {
         return node1.isBottomState() && node2.isBottomState();
     }
     private static boolean checkUpRight(Node node1, Node node2){
-        if (!node1.isTop_rightState() && !node2.isTop_leftState()) {
-            return node1.getTop().isRightState() && node2.getTop().isLeftState();
-        }
-        return false;
+        return node1.getTop().isRightState() && node2.getTop().isLeftState();
     }
     private static boolean checkUpLeft(Node node1, Node node2){
-        if (!node1.isTop_leftState() && !node2.isTop_rightState() ) {
             return node1.getTop().isLeftState() && node2.getTop().isRightState();
-        }
-        return false;
     }
     private static boolean checkDownRight(Node node1, Node node2){
-        if (!node1.isBottom_rightState() && !node2.isBottom_leftState()) {
             return node1.getBottom().isRightState() && node2.getBottom().isLeftState();
-        }
-        return false;
     }
     private static boolean checkDownLeft(Node node1, Node node2){
-        if (!node1.isBottom_leftState() && !node2.isBottom_rightState()) {
-            return node1.getBottom().isLeftState() && node2.getBottom().isRightState();
-        }
-        return false;
+        return node1.getBottom().isLeftState() && node2.getBottom().isRightState();
     }
 
     public static boolean checkverticals(Node node1, Node node2){
         if (node1.getRight() == node2){
             if(checkUp(node1, node2)){
-                return checkUpRight(node1, node2);
+                if (!node1.isTop_rightState() && !node2.isTop_leftState()) {
+                    return checkUpRight(node1, node2);
+                } else {
+                    return node2.isTop_leftState();
+                }
             }
             if (checkDown(node1, node2)){
-                return checkDownRight(node1, node2);
+                if (!node1.isTop_rightState() && !node2.isBottom_leftState()) {
+                    return checkDownRight(node1, node2);
+                } else {
+                    return node2.isBottom_leftState();
+                }
             }
         }
         if (node1.getLeft() == node2){
            if (checkUp(node1, node2)){
-               return checkUpLeft(node1, node2);
+               if (!node1.isTop_leftState() && !node2.isTop_rightState()) {
+                   return checkUpLeft(node1, node2);
+               } else {
+                   return node2.isTop_rightState();
+               }
            }
            if (checkDown(node1, node2)){
-               return checkDownLeft(node1, node2);
+               if (!node1.isBottom_leftState() && !node2.isBottom_rightState()) {
+                   return checkDownLeft(node1, node2);
+               } else {
+                   return node2.isBottom_rightState();
+               }
            }
         }
         return false;
