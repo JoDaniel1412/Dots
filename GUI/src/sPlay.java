c864a28e1a13a6e6c219c5552e0e85f979d743fc
JoDaniel1412 <jodaniel1409@gmail.com>
2018-09-13 12:03:25 -0600

    Added id to the buttons in sPlay

--- GUI/src/sPlay.java
+++ GUI/src/sPlay.java
@@ -1,8 +1,23 @@
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 
 import java.io.IOException;
 
 public class sPlay extends sScene{
+
+    @FXML
+    private Button bBoardSize1;
+
+    @FXML
+    private Button bBoardSize2;
+
+    @FXML
+    private Button bBoardSize3;
+
+    @FXML
+    private void pressed_bBoardSize1(){
+
+    }
 
     @Override
     void pressed_return() throws IOException {
@@ -10,8 +25,8 @@ public class sPlay extends sScene{
     }
 
     @FXML
-    void pressed_setGridSize(int rows, int columns){
-        System.out.printf("%d %d", rows, columns);
+    void pressed_setGridSize(){
+
     }
 
     @FXML
