8fd292b418b47468bcf25b267dc9e4e7a44d0679
JoDaniel1412 <jodaniel1412@gmail.com>
2018-09-15 18:01:25 -0600

    Added libraries to sGame

--- GUI/src/sGame.java
+++ GUI/src/sGame.java
@@ -2,9 +2,13 @@ import drawings.Dots;
 import javafx.fxml.FXML;
 import javafx.scene.Group;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Background;
+import javafx.scene.layout.BackgroundFill;
 import lists.Board;
 import lists.Node;
 import javafx.scene.canvas.Canvas;
+
+import java.awt.*;
 
 public class sGame {
 
