78bb9b578ebbdc4a46aeec29a64580d5c01332d8
[Fabian] <[ramirezfabian003@gmail.com]>
2018-09-18 12:16:05 -0600

    fixed triangles made after a horizontal line

--- GameLogic/src/PointsChecker/verticalChecker.java
+++ GameLogic/src/PointsChecker/verticalChecker.java
@@ -7,8 +7,6 @@ import lists.Node;
 public class verticalChecker {
 
     private static boolean checkUp(Node node1, Node node2){
-        System.out.println(node1.isTopState());
-        System.out.println(node2.isTopState());
         return node1.isTopState() && node2.isTopState();
     }
     private static boolean checkDown(Node node1, Node node2){
@@ -26,23 +24,72 @@ public class verticalChecker {
     private static boolean checkDownLeft(Node node1, Node node2){
         return node1.getBottom().isLeftState() && node2.getBottom().isRightState();
     }
+    private static boolean checkRighDiagonal(Node node1, Node node2) {
+        boolean result = false;
+        if (node1.isBottom_rightState() && node1.isTop_rightState()) { //puntos dobles
+            System.out.println("Doble 1");
+            result = true;
+        }
+        if (node2.isTop_leftState() && node2.isBottom_leftState()) {  // puntos dobles
+            System.out.println("Doble 2");
+            result = true;
+        }
+        if (!node1.isBottom_rightState() && node1.isTop_rightState()) {
+            result = true;
+        }
+        if (node1.isBottom_rightState() && !node1.isTop_rightState()) {
+            result = true;
+        }
+        if (!node2.isTop_leftState() && node2.isBottom_leftState()) {
+            result = true;
+        }
+        if (node2.isTop_leftState() && !node2.isBottom_leftState()) {
+            result = true;
+        }
+        return result;
+    }
+
+    private static boolean checkLeftDiagonal(Node node1, Node node2) {
+        boolean result = false;
+        if (node1.isBottom_leftState() && node1.isTop_leftState()) { //puntos dobles
+            System.out.println("Doble 1");
+            result = true;
+        }
+        if (node2.isTop_rightState() && node2.isBottom_rightState()){  // puntos dobles
+            System.out.println("Doble 2");
+            result = true;
+        }
+        if (!node1.isBottom_leftState() && node1.isTop_leftState()) {
+            result = true;
+        }
+        if (node1.isBottom_leftState() && !node1.isTop_leftState()) {
+            result = true;
+        }
+        if (!node2.isTop_rightState() && node2.isBottom_rightState()) {
+            result = true;
+        }
+        if (node2.isTop_rightState() && !node2.isBottom_rightState()) {
+            result = true;
+        }
+        return result;
+    }
 
     public static boolean checkverticals(Node node1, Node node2){
-        System.out.println(node1.getRight());
-        System.out.println(node2);
         if (node1.getRight() == node2){
             if(checkUp(node1, node2)){
-                if (!node1.isTop_rightState() && !node2.isTop_leftState()) {
+                System.out.println("UP");
+                if (!node1.isTop_rightState() && !node2.isTop_leftState() && !node1.getTop().isBottom_rightState() && !node2.getTop().isTop_leftState()) {
+                    System.out.println("Diagonal");
                     return checkUpRight(node1, node2);
                 } else {
-                    return node2.isTop_leftState();
+                    return verticalChecker.checkRighDiagonal(node1, node2);
                 }
             }
             if (checkDown(node1, node2)){
                 if (!node1.isTop_rightState() && !node2.isBottom_leftState()) {
                     return checkDownRight(node1, node2);
                 } else {
-                    return node2.isBottom_leftState();
+                    return verticalChecker.checkLeftDiagonal(node1, node2);
                 }
             }
         }
