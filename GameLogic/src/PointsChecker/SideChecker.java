828b90ec346419234db30dfb455cf95b1067fc0e
[Fabian] <[ramirezfabian003@gmail.com]>
2018-09-19 12:06:08 -0600

    Verify triangles and squares with a single line in Sidechecker class

--- GameLogic/src/PointsChecker/SideChecker.java
+++ GameLogic/src/PointsChecker/SideChecker.java
@@ -82,57 +82,90 @@ public class SideChecker {
         return result;
     }
 
+    private static boolean noDiagonalsUp (Node node1, Node node2){
+        return (!node1.isTop_rightState() && !node1.isTop_leftState() && !node2.isBottom_leftState() && !node2.isBottom_rightState());
+    }
+    private static boolean noDiagonalsDown (Node node1, Node node2){
+        return (!node1.isBottom_rightState() && !node1.isBottom_leftState() && !node2.isTop_leftState() && !node2.isTop_rightState());
+    }
 
     public static boolean CheckSides(Node node1, Node node2) {
         if (node1.getTop() == node2) {
             if (CheckRight(node1, node2) && CheckLeft(node1, node2)){
-                if (CheckLeftUp(node1, node2) && CheckRightUp(node1, node2)){
-                    System.out.println("Doble");
+                if (CheckLeftUp(node1, node2) && CheckRightUp(node1, node2) && noDiagonalsUp(node1, node2)){
                     return true;
                 }
             }
 
-            if (SideChecker.checkUpDiagonal(node1, node2)){
-                return true;
-            }
-
             if (CheckRight(node1, node2)) {
                 if (!node1.isTop_rightState() && !node2.isBottom_rightState()) {
-                    return CheckRightUp(node1, node2);
+                    if(CheckRightUp(node1, node2)){
+                        if (node2.isLeftState() && node1.isTop_leftState()) {
+                            return true;
+                        }
+                        if (node1.isLeftState() && node2.isBottom_leftState()) {
+                            return true;
+                        }
+                        return true;
+                    }
                 }
 
             }
 
             if (CheckLeft(node1, node2)){
                 if (!node1.isTop_leftState() && !node2.isBottom_rightState()) {
-                    return CheckLeftUp(node1, node2);
-                } else {
-                    return node2.isTop_leftState(); //Triangle point
+                    if(CheckLeftUp(node1, node2)){
+                        if (node2.isRightState() && node1.isTop_rightState()) {
+                            return true;
+                        }
+                        if (node1.isRightState() && node2.isBottom_rightState()) {
+                            return true;
+                        }
+                        return true;
+                    }
                 }
             }
-
+            if (SideChecker.checkUpDiagonal(node1, node2)){
+                return true;
+            }
         }
 
         if (node1.getBottom() == node2){
-            if (CheckRight(node1, node2) && CheckLeft(node1, node2)){
-                if (CheckLeftDown(node1, node2) && CheckRightDown(node1, node2)){
-                    System.out.println("Doble");
+            if (CheckRight(node1, node2) && CheckLeft(node1, node2) && noDiagonalsDown(node1, node2)){
+                if (CheckLeftDown(node1, node2) && CheckRightDown(node1, node2) && noDiagonalsDown(node1, node2)){
                     return true;
                 }
             }
-            if (SideChecker.checkDownDiagonal(node1, node2)){
-                return true;
-            }
+
             if (CheckRight(node1, node2)){
                 if (!node1.isBottom_rightState() && !node2.isTop_rightState()) {
-                    return CheckRightDown(node1, node2);
-                } else {
-                    return node2.isTop_rightState();
+                    if(CheckRightDown(node1, node2)){
+                        if (node2.isLeftState() && node1.isTop_leftState()) {
+                            return true;
+                        }
+                        if (node1.isLeftState() && node2.isBottom_leftState()) {
+                            return true;
+                        }
+                        return true;
+                    }
                 }
 
             }
             if (CheckLeft(node1, node2)){
-                return CheckLeftDown(node1, node2);
+                if (!node1.isBottom_leftState() && !node2.isTop_leftState()) {
+                    if(CheckLeftDown(node1, node2)){
+                        if (node2.isRightState() && node1.isTop_rightState()) {
+                            return true;
+                        }
+                        if (node1.isRightState() && node2.isBottom_rightState()) {
+                            return true;
+                        }
+                        return true;
+                    }
+                }
+            }
+            if (SideChecker.checkDownDiagonal(node1, node2)){
+                return true;
             }
         }
         return false;
