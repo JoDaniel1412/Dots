7c2a27b72d18499c61361c738777a91bdd9e5317
Jesus <sandoval14yamir@hotmail.com>
2018-09-26 21:29:58 -0600

    Creo la clase SoundsEffects

--- GUI/src/scenes/sSettings.java
+++ GUI/src/scenes/sSettings.java
@@ -2,12 +2,16 @@ package scenes;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Slider;
+import javafx.scene.media.Media;
+import javafx.scene.media.MediaPlayer;
 
 import java.io.IOException;
 
 public class sSettings extends sScene {
 @FXML
 Slider volumeSlider;
+private MediaPlayer mp;
+private Media me;
     @Override
     void pressed_return()  {
         MainInterface.setScene("fxml/menu.fxml");
